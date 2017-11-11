d005037608ab938c3d5715a6ac0375984ef9234f
John Doe <johndoe@example.com>
2017-11-11 13:09:11 +0200

    division

--- javascript/script.js
+++ javascript/script.js
@@ -1,69 +1,13 @@
-// var namber1 = +prompt("Введите первое число");
-// var namber2 = +prompt("Введите второе число");
-// if (isNaN(namber1) || isNaN(namber2)) {
-// 	alert("не число")
-// }else {
-// (namber1 % namber2 == 0) ? alert("делится без остатка") : alert("не делится без остатка");
-// }
+var namber1 = +prompt("Введите первое число");
+var namber2 = +prompt("Введите второе число");
+if (isNaN(namber1) || isNaN(namber2)) {
+	alert("не число")
+}else {
+(namber1 % namber2 == 0) ? alert("делится без остатка") : alert("не делится без остатка");
+}
 
 
-// var season = prompt("введите месяц своего рождения");
-// switch(season) {
-// 	case "12":
-// 	case "1":
-// 	case "2":
-// 	case "декабрь":
-// 	case "январь":
-// 	case "февраль":
-// 		alert("зима");
-// 		break;
-// 	case "3":
-// 	case "4":
-// 	case "5":
-// 	case "март":
-// 	case "апрель":
-// 	case "май":
-// 		alert("весна");
-// 		break;
-// 	case "6":
-// 	case "7":
-// 	case "8":
-// 	case "июнь":
-// 	case "июль":
-// 	case "август":
-// 		alert("лето");
-// 		break;
-// 	case "9":
-// 	case "10":
-// 	case "11":
-// 	case "сентябрь":
-// 	case "октябрь":
-// 	case "ноябрь":
-// 		alert("осень");
-// 		break;
-// 	default:
-// 		alert("Ошибка");
-// }
-
-	
-//  	var number = 5;
-//  	var nakopitel = 0;
-//  while (number != enter){
-// 	 var enter = +prompt("введите число от 0 до 100");
-// 	if (enter == number) {
-// 		alert("Поздровляю вы угадали c " + nakopitel + " Попытки");
-// 	}else if (isNaN(enter) || enter > 100 || enter < 0) {
-// 		alert("вы ввели некоректные данные");
-// 	}else if (enter > 5){
-// 		alert("вы ввели число больше загаданного");
-// 	}else if (enter < 5){
-// 		alert("Вы ввели число меньше загаданного");
-// 	}else {
-// 		alert("Вы не угадали число");
-// 	}
-// 	nakopitel++;
-// }
-// ________________________________________________________________________
+____________________________________
 // ТО ЧТО ДАВАЛОСЬДЕЛАТЬ В ТЕЛЕГЕ СНИЗУ
 
 // var accumulate = 0;
